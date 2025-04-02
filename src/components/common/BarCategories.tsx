a9b64d6e48ec8329358d0c4d913e9a2155c5aac0
adair <adairaxe31@hotmail.com>
2025-04-01 23:09:20 -0500

    delete: css

--- src/components/common/BarCategories.tsx
+++ src/components/common/BarCategories.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function BarCategories() {
     return (
         <>
-            <nav style={{display: "flex", justifyContent: "space-around"}}>
-                <Link to={"/peliculas"}>Peliculas</Link>
-                <Link to={"/libros"}>Libros</Link>
+            <nav style={{ display: "flex", justifyContent: "space-around" }}>
+                <Link to={"/peliculas"}><h2>Peliculas</h2></Link>
+                <Link to={"/libros"}><h2>Libros</h2></Link>
             </nav>
         </>
     )
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,7 +4,8 @@ import "../../styles/header.css"
 function Header() {
     return (
         <header>
-            <Link to="/"><h1>Escritos memorables</h1></Link>
+            <Link to="/"><h1>Frases memorables</h1></Link>
+            <h2>Estos escritos han impactado fuertemente en mí y por esa razón los comparto en este sitio</h2>
         </header>
     )
 }
